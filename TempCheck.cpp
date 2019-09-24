50450168685d7e35cd5b92d6835d39eb9a65bc88
rishitshivesh <55372921+rishitshivesh@users.noreply.github.com>
2019-09-24 22:44:22 +0530

    Update TempCheck.cpp

--- TempCheck.cpp
+++ TempCheck.cpp
@@ -13,8 +13,11 @@ class CUSTOMER
 	char Password[50];
 public:
 	char Username[50];
+	char Name[50], EMail[25];
+	char Phone[10];
+
 	//CUSTOMER(char *user,char *pass);
-	void inputdata(char *username,char *password);
+	void inputdata(char *name, char *username,char *password, char *email, char *phone);
 	int CheckPassword(char *password);
 
 
@@ -27,19 +30,30 @@ int CUSTOMER::CheckPassword(char *password)
 		return 0;
 	}
 }
-void CUSTOMER::inputdata(char *username,char *password)
+void CUSTOMER::inputdata(char *name, char *username,char *password, char *email, char *phone)
 {
+	strcpy(Name,name);
 	strcpy(Username,username);
 	strcpy(Password,password);
+	strcpy(EMail,email);
+	strcpy(Phone,phone);
 }
 Window *awelcome = new Window(10,10,630,480,"WELCOME");
 // Button *standard[5];
 // Button *bwelcome[2];
 Button *back = new Button(20,400,100,30,"BACK",1);
 Button *next = new Button(500,400,100,30,"NEXT",2);
-TextBox *pusername = new TextBox(200,200,243,30,"USERNAME",LEFT_TEXT,1);
-TextBox *ppassword = new TextBox(200,240,243,30,"PASSWORD",LEFT_TEXT,2);
-
+//TextBox *pusername = new TextBox(200,200,243,30,"USERNAME",LEFT_TEXT,1);
+//TextBox *ppassword = new TextBox(200,240,243,30,"PASSWORD",LEFT_TEXT,2);
+int LoginStatus=0;
+void Customerlogin();
+/*int CheckLoginStatus()
+{
+	if(LoginStatus==0)
+		return 0;
+	else
+		return 1;
+}*/
 void welcome();
 void Customerlogin();
 void CustomerTime();
@@ -55,7 +69,7 @@ int CheckFile(char *username,char *password=" ")
 			exists=1;
 		else if(strcmp(Customer.Username,username)==0)
 			exists=2;
-		
+
 	}
 
 	fil.close();
@@ -82,6 +96,8 @@ void AdminLogin()
 	cleardevice();
 	Window *pLogin = new Window(10,10,630,480,"ADMIN LOGIN");
 	pLogin->Draw();
+	TextBox *pusername = new TextBox(200,200,243,30,"USERNAME",LEFT_TEXT,1);
+	TextBox *ppassword = new TextBox(200,240,243,30,"PASSWORD",LEFT_TEXT,2);
     pusername->Draw();
     ppassword->Draw();
     back->Draw();
@@ -94,8 +110,8 @@ void AdminLogin()
 	while(1)
 	{
 		mouse.ShowMouse(); // To show mouse
-        mouse.GetMouseStatus(); // To get position of mouse
-        pLogin->EnableClickHandler();
+	mouse.GetMouseStatus(); // To get position of mouse
+	pLogin->EnableClickHandler();
 		pusername->EnableClickHandler();
 		ppassword->EnableClickHandler();
 		ppassword->HideText(true);
@@ -107,9 +123,9 @@ void AdminLogin()
 		// }
 		if(ButtonId==1)
 	    {
-            welcome();
+	    welcome();
 	    }
-	} 
+	}
 }
 void CustomerHome()
 {
@@ -164,18 +180,27 @@ void CustomerHome()
 			CustomerTime();
 		}
 
-		else if(ButtonId==1)
+		else if(ButtonId==1&&LoginStatus==1)
 		{
 			int msgBtn = msgbox.MessageBox(200,200,"HOME","Are you sure you want to \n  Log Out",BLUE,jcYesNo,jcCritical);
 				if(msgBtn=jcYes)
 				{
+					LoginStatus=0;
 					Customerlogin();
+
 				}
 				else{CustomerHome();}
 
 		}
+		else if(ButtonId==1&&LoginStatus==0)
+		{
+			welcome();
+		}
+		else
+		{
+		}
 
-		else{}
+
 
     }
 }
@@ -204,15 +229,15 @@ void CustomerTime()
 	t7->Draw();
 	t8->Draw();
 	back->Draw();
-	next->Draw();
 	// for(int i=1;i<=2;i++)
     // {
     // 	bwelcome[i]->Draw();
     // }
     while(1)
     {
-	mouse.ShowMouse(); // To show mouse
-	mouse.GetMouseStatus(); // To get position of mouse
+		int Chk=LoginStatus;
+		mouse.ShowMouse(); // To show mouse
+		mouse.GetMouseStatus(); // To get position of mouse
 		awelcome->EnableClickHandler();
 		t1->EnableClickHandler();
 		t2->EnableClickHandler();
@@ -223,52 +248,74 @@ void CustomerTime()
 		t7->EnableClickHandler();
 		t8->EnableClickHandler();
 		back->EnableClickHandler();
-		next->EnableClickHandler();
    //  	for(int i=1;i<=2;i++)
 			// bwelcome[i]->EnableClickHandler();
 		if(ButtonId==1)
 			CustomerHome();
-		else if(ButtonId==3)
+		else if(ButtonId==3&&Chk==1)
 			strcpy(Movie.Timing,t1->GetText());
-		else if(ButtonId==4)
+		else if(ButtonId==4&&Chk==1)
 			strcpy(Movie.Timing,t2->GetText());
-		else if(ButtonId==5)
+		else if(ButtonId==5&&Chk==1)
 			strcpy(Movie.Timing,t3->GetText());
-		else if(ButtonId==6)
+		else if(ButtonId==6&&Chk==1)
 			strcpy(Movie.Timing,t4->GetText());
-		else if(ButtonId==7)
+		else if(ButtonId==7&&Chk==1)
 			strcpy(Movie.Timing,t5->GetText());
-		else if(ButtonId==8)
+		else if(ButtonId==8&&Chk==1)
 			strcpy(Movie.Timing,t6->GetText());
-		else if(ButtonId==9)
+		else if(ButtonId==9&&Chk==1)
 			strcpy(Movie.Timing,t7->GetText());
-		else if(ButtonId==10)
+		else if(ButtonId==10&&Chk==1)
 			strcpy(Movie.Timing,t8->GetText());
-		else if(ButtonId==2)
-			CustomerTime();
+		else if(ButtonId==3||ButtonId==4||ButtonId==5||ButtonId==6||ButtonId==7||ButtonId==8||ButtonId==9||ButtonId==10&&Chk==0)
+		{
 
-		else{}
-    }
+			int msgBtn = msgbox.MessageBox(200,200,"Credentials Needed","Please Login to Proceed",BLUE,jcOKOnly,jcCritical);
+			if(msgBtn=jcOK)
+			{
+				Customerlogin();
+			}
+
+		}
+
+		}
+
 }
 void CustomerSignUp()
 {
 	cleardevice();
 	Window *pSignUpWindow = new Window(10,10,630,480,"CUSTOMER SIGN UP");
+	Label *page = new Label(200,70,243,30,"Customer Sign Up",CENTER_TEXT);
+	TextBox *pname = new TextBox(200,130,243,30,"NAME",LEFT_TEXT,1);
+	TextBox *pusername = new TextBox(200,180,243,30,"USERNAME",LEFT_TEXT,2);
+	TextBox *ppassword = new TextBox(200,230,243,30,"PASSWORD",LEFT_TEXT,3);
+	TextBox *pphone = new TextBox(200,280,243,30,"PHONE NUMBER",LEFT_TEXT,4);
+	TextBox *pemail = new TextBox(200,330,243,30,"EMAIL ID",LEFT_TEXT,5);
 
 	pSignUpWindow->Draw();
+	page->Draw();
+	pname->Draw();
 	pusername->Draw();
-    ppassword->Draw();
+	ppassword->Draw();
+	pphone->Draw();
+	pemail->Draw();
+
     back->Draw();
     next->Draw();
     while(1)
     {
-	
+
 		mouse.ShowMouse(); // To show mouse
 		mouse.GetMouseStatus(); // To get position of mouse
 		pSignUpWindow->EnableClickHandler();
-		pusername->EnableClickHandler();
 		ppassword->EnableClickHandler();
+		pname->EnableClickHandler();
+		pusername->EnableClickHandler();
 		ppassword->HideText(true);
+		pphone->EnableClickHandler();
+		pemail->EnableClickHandler();
+
 		back->EnableClickHandler();
 		next->EnableClickHandler();
 
@@ -279,7 +326,8 @@ void CustomerSignUp()
 		if(ButtonId==2)
 		{
 			CUSTOMER Customer;
-			Customer.inputdata(pusername->GetText(),ppassword->GetText());
+
+			Customer.inputdata(pname->GetText(),pusername->GetText(),ppassword->GetText(),pemail->GetText(),pphone->GetText());
 			if(CheckFile(pusername->GetText())==2)
 			{
 				int msgBtn = msgbox.MessageBox(200,200,"Sign Up","User Exists. Please Login!",BLUE,jcOKOnly,jcCritical);
@@ -307,6 +355,8 @@ void Customerlogin()
 	Window *pLogin = new Window(10,10,630,480,"CUSTOMER LOGIN");
 	Button *pCustomerSignUp = new Button(200,400,243,30,"New Here, SIGN UP",5);
 	pLogin->Draw();
+	TextBox *pusername = new TextBox(200,200,243,30,"USERNAME",LEFT_TEXT,2);
+	TextBox *ppassword = new TextBox(200,240,243,30,"PASSWORD",LEFT_TEXT,3);
     pusername->Draw();
     ppassword->Draw();
     pCustomerSignUp->Draw();
@@ -336,7 +386,10 @@ void Customerlogin()
 	    if(ButtonId==2)
 	    {
 			if(CheckFile(pusername->GetText(),ppassword->GetText())==1)
+			{
+				LoginStatus=1;
 				CustomerHome();
+			}
 			else
 			{
 				int msgBtn = msgbox.MessageBox(200,200,"New","Incorrect Password or Username",BLUE,jcRetryCancel,jcCritical);
@@ -345,7 +398,7 @@ void Customerlogin()
 						//fil.close();
 					}
 			}
-			
+
 	    }
 	    if(ButtonId==5)
 	    {
@@ -358,10 +411,12 @@ void Customerlogin()
 void welcome()
 {
 	cleardevice();
-	Button *admin = new Button(200,200,243,30,"ADMIN",3);
-	Button *customer = new Button(200,240,243,30,"CUSTOMER",4);
+	Button *admin = new Button(200,180,243,30,"ADMIN",3);
+	Button *customer = new Button(200,220,243,30,"CUSTOMER",4);
+	Button *guest = new Button(200,260,243,30,"GUEST",5);
 	awelcome->Draw();
 	admin->Draw();
+	guest->Draw();
 	customer->Draw();
 
     // for(int i=1;i<=2;i++)
@@ -375,12 +430,16 @@ void welcome()
 		awelcome->EnableClickHandler();
 		admin->EnableClickHandler();
 		customer->EnableClickHandler();
+		guest->EnableClickHandler();
    //  	for(int i=1;i<=2;i++)
 			// bwelcome[i]->EnableClickHandler();
 		if(ButtonId==3)//Admin
-		AdminLogin();
-	if(ButtonId==4)//Customer
-		Customerlogin();
+			AdminLogin();
+		else if(ButtonId==4)//Customer
+			Customerlogin();
+		else if(ButtonId==5)
+			CustomerHome();
+
 
     }
 }
